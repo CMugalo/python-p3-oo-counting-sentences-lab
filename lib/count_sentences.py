2e11ff001ee914efe3103139fb7d53d499d22107
Christian Mugalo <christian.mugalo@student.moringaschool.com>
2024-07-02 07:32:57 +0300

    only done the first deliverable

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,23 @@
 #!/usr/bin/env python3
 
 class MyString:
-  pass
+  def __init__(self, value = ""):
+    self.value = value
+  
+  @property
+  def value(self):
+    return self._value
+  
+  @value.setter
+  def value(self, value):
+    if type(value) == str:
+      self._value = value
+    else:
+      print("The value must be a string.")
+  
+  # def is_sentence(self, value):
+  #   if type(value) == str and value[-1] == ".":
+  #     self._value = value
+  #     return True
+  #   else:
+  #     return False
